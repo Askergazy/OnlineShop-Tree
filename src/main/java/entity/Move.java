a6a15d7812f48881fa642940712b2da3b900391f
Askergazy <askaraskergazy@gmail.com>
2023-02-24 19:56:25 +0600

    changes in Move

--- src/main/java/entity/Move.java
+++ src/main/java/entity/Move.java
@@ -1,6 +1,7 @@
 package entity;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.Scanner;
 
 
@@ -24,60 +25,106 @@ public class Move {
 
         try {
             manager.getTransaction().begin();
+                if ( targetId > 0) {
+                    Tree tree = manager.find(Tree.class,sourceId);
 
-            Tree tree = manager.find(Tree.class,sourceId);
+                    Integer upd = tree.getRightKey() - tree.getLeftKey() +1 ;
+                    System.out.println(upd + "upd");
 
-
-
-            Integer upd = tree.getRightKey() - tree.getLeftKey() +1 ;
-            System.out.println(upd + "upd");
-
-            TypedQuery<Tree> queryCount= manager.createQuery("select (t)  from Tree t where t.rightKey <= ?1 and t.leftKey >= ?2",Tree.class);
-            queryCount.setParameter(1,tree.getRightKey());
-            queryCount.setParameter(2,tree.getLeftKey());
+                    TypedQuery<Tree> queryCount= manager.createQuery("select (t)  from Tree t where t.rightKey <= ?1 and t.leftKey >= ?2",Tree.class);
+                    queryCount.setParameter(1,tree.getRightKey());
+                    queryCount.setParameter(2,tree.getLeftKey());
 
 
-            Query query = manager.createQuery("update Tree t set t.rightKey = t.rightKey * -1,t.leftKey = t.leftKey * -1 where t.rightKey <= ?1 and t.leftKey >= ?2 ");
-            query.setParameter(1,tree.getRightKey());
-            query.setParameter(2,tree.getLeftKey());
-            query.executeUpdate();// Перевод в отриц-е
+                    Query query = manager.createQuery("update Tree t set t.rightKey = t.rightKey * -1,t.leftKey = t.leftKey * -1 where t.rightKey <= ?1 and t.leftKey >= ?2 ");
+                    query.setParameter(1,tree.getRightKey());
+                    query.setParameter(2,tree.getLeftKey());
+                    query.executeUpdate();// Перевод в отриц-е
 
-            Query rk  = manager.createQuery("update Tree t set t.rightKey = t.rightKey - ?1 where  t.rightKey >= ?2");
-            rk.setParameter(1,upd);
-            rk.setParameter(2,tree.getRightKey());
-            rk.executeUpdate();
+                    Query rk  = manager.createQuery("update Tree t set t.rightKey = t.rightKey - ?1 where  t.rightKey >= ?2");
+                    rk.setParameter(1,upd);
+                    rk.setParameter(2,tree.getRightKey());
+                    rk.executeUpdate();
 
-            Query lk = manager.createQuery("update Tree t set t.leftKey = t.leftKey - ?1 where   t.leftKey >= ?2");
-            lk.setParameter(1,upd);
-            lk.setParameter(2,tree.getRightKey());
-            lk.executeUpdate();//Закрытие промежутка
+                    Query lk = manager.createQuery("update Tree t set t.leftKey = t.leftKey - ?1 where   t.leftKey >= ?2");
+                    lk.setParameter(1,upd);
+                    lk.setParameter(2,tree.getRightKey());
+                    lk.executeUpdate();//Закрытие промежутка
 
-            Tree  treeIn = manager.find(Tree.class,targetId);
-            Integer rKey = treeIn.getRightKey();
+                    Tree  treeIn = manager.find(Tree.class,targetId);
+                    Integer rKey = treeIn.getRightKey();
 
 
-            Query moveBelow = manager.createQuery("update Tree t set t.rightKey = t.rightKey + ?1  where t.rightKey >= ?2 ");
-            moveBelow.setParameter(1,upd);
-            moveBelow.setParameter(2,rKey);
-            moveBelow.executeUpdate();
+                    Query moveBelow = manager.createQuery("update Tree t set t.rightKey = t.rightKey + ?1  where t.rightKey >= ?2 ");
+                    moveBelow.setParameter(1,upd);
+                    moveBelow.setParameter(2,rKey);
+                    moveBelow.executeUpdate();
 
-            Query moveAbove = manager.createQuery("update Tree t set t.leftKey = t.leftKey + ?1 where t.leftKey > ?2");
-            moveAbove.setParameter(1,upd);
-            moveAbove.setParameter(2,rKey);
-            moveAbove.executeUpdate();
+                    Query moveAbove = manager.createQuery("update Tree t set t.leftKey = t.leftKey + ?1 where t.leftKey > ?2");
+                    moveAbove.setParameter(1,upd);
+                    moveAbove.setParameter(2,rKey);
+                    moveAbove.executeUpdate();
 
-            Query inAfter = manager.createQuery("select t.rightKey from Tree t where t.id = ?1");
-            inAfter.setParameter(1,targetId);
-            System.out.println(inAfter.getSingleResult());
+                    Query inAfter = manager.createQuery("select t.rightKey from Tree t where t.id = ?1");
+                    inAfter.setParameter(1,targetId);
+                    System.out.println(inAfter.getSingleResult());
 
-            Query targetAfter = manager.createQuery("select t.rightKey from Tree t where t.id =?1");
-            targetAfter.setParameter(1,sourceId);
+                    Query targetAfter = manager.createQuery("select t.rightKey from Tree t where t.id =?1");
+                    targetAfter.setParameter(1,sourceId);
+                    System.out.println(targetAfter.getSingleResult());
 
 //
-           Query add =  manager.createQuery("update Tree t set t.rightKey = 0 - (t.rightKey) + (?1 - (0 - (?2)) - 1),t.leftKey = 0 - (t.leftKey) + (?1 - (0 - (?2)) - 1) where t.rightKey <0");
-           add.setParameter(1,inAfter.getSingleResult());
-           add.setParameter(2,targetAfter.getSingleResult());
-           add.executeUpdate();
+                    Query add =  manager.createQuery("update Tree t set t.rightKey = 0 - (t.rightKey) + (?1 - (0 - (?2)) - 1),t.leftKey = 0 - (t.leftKey) + (?1 - (0 - (?2)) - 1) where t.rightKey <0");
+                    add.setParameter(1,inAfter.getSingleResult());
+                    add.setParameter(2,targetAfter.getSingleResult());
+                    add.executeUpdate();
+                }else {
+
+
+                    TypedQuery<Integer> maxRk = manager.createQuery("select max (t.rightKey) from Tree t",Integer.class);
+                    Tree tree = manager.find(Tree.class,sourceId);
+
+
+                    Integer upd = tree.getRightKey() - tree.getLeftKey() +1 ;
+                    System.out.println(upd + "upd");
+
+                    TypedQuery<Tree> queryCount= manager.createQuery("select (t)  from Tree t where t.rightKey <= ?1 and t.leftKey >= ?2",Tree.class);
+                    queryCount.setParameter(1,tree.getRightKey());
+                    queryCount.setParameter(2,tree.getLeftKey());
+
+
+                    Query query = manager.createQuery("update Tree t set t.rightKey = t.rightKey * -1,t.leftKey = t.leftKey * -1 where t.rightKey <= ?1 and t.leftKey >= ?2 ");
+                    query.setParameter(1,tree.getRightKey());
+                    query.setParameter(2,tree.getLeftKey());
+                    query.executeUpdate();// Перевод в отриц-е
+
+                    Query rk  = manager.createQuery("update Tree t set t.rightKey = t.rightKey - ?1 where  t.rightKey >= ?2");
+                    rk.setParameter(1,upd);
+                    rk.setParameter(2,tree.getRightKey());
+                    rk.executeUpdate();
+
+                    Query lk = manager.createQuery("update Tree t set t.leftKey = t.leftKey - ?1 where   t.leftKey >= ?2");
+                    lk.setParameter(1,upd);
+                    lk.setParameter(2,tree.getRightKey());
+                    lk.executeUpdate();//Закрытие промежутка
+
+
+
+                    Query targetAfter = manager.createQuery("select t.leftKey from Tree t where t.id =?1");
+                    targetAfter.setParameter(1,sourceId);
+                    System.out.println(targetAfter.getSingleResult());
+
+//
+                    Query add =  manager.createQuery("update Tree t set t.rightKey = 0 - (t.rightKey) + (?1 - (0 - (?2)) + 1),t.leftKey = 0 - (t.leftKey) + (?1 - (0 - (?2)) + 1),t.level =  t.level - 1 where t.rightKey <0");
+                    add.setParameter(1,maxRk.getSingleResult());
+                    add.setParameter(2,targetAfter.getSingleResult());
+                    add.executeUpdate();
+
+
+
+
+                }
+
 
             // -2 -7 -> 5 18 -> 12 17
             // -3 -4            13 14
